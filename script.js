4154eaf2315e0e22626c403d9fbc1afeba9144f3
abarre <a@fasterize.com>
2013-01-04 10:40:37 +0100

    Reindent the manifest + run_at document_idle

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-window.onload = function (){
-  var d = document.createElement("textarea"),
-      e = document.createEvent("Events");
-  d.style.cssText = "display:none;";
-  d.value = window.fstrz;
-  d.addEventListener("action", function() {
-      d.parentNode.removeChild(d);
-  }, true);
-  document.body.appendChild(d);
+var d = document.createElement("textarea"),
+    e = document.createEvent("Events");
+d.style.cssText = "display:none;";
+d.value = window.fstrz;
+d.addEventListener("action", function() {
+    d.parentNode.removeChild(d);
+}, true);
+document.body.appendChild(d);
 
-  // Fire events, to notify the Content script
-  e.initEvent("RW759_connectExtension", false, true);
-  d.dispatchEvent(e);
-}
+// Fire events, to notify the Content script
+e.initEvent("RW759_connectExtension", false, true);
+d.dispatchEvent(e);
