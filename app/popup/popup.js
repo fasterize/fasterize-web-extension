8e8bd233d295369770a429d4066a319c3d5014b8
Anthony BARRE <a@fasterize.com>
2022-03-16 12:20:06 +0100

    style: apply prettier

--- app/popup/popup.js
+++ app/popup/popup.js
@@ -1,20 +1,21 @@
 function logError(e) {
-  console.log(browser.runtime.lastError, e)
+  console.log(e);
+  console.log(browser.runtime.lastError, e);
 }
 
 function setFstrzCookie(url, value) {
-  const tmp = document.createElement('a')
-  tmp.href = url
+  const tmp = document.createElement('a');
+  tmp.href = url;
 
   let rootDomain = `.${tmp.hostname
     .split('.')
     .slice(-2)
-    .join('.')}`
+    .join('.')}`;
   if (rootDomain === '.co.uk' || rootDomain === '.com.tr') {
     rootDomain = `.${tmp.hostname
       .split('.')
       .slice(-3)
-      .join('.')}`
+      .join('.')}`;
   }
 
   return Promise.all([
@@ -30,209 +31,202 @@ function setFstrzCookie(url, value) {
       name: 'frz-forced-state',
       value: 'true',
     }),
-  ])
+  ]);
 }
 
 function getFstrzCookie(url) {
-  const tmp = document.createElement('a')
-  tmp.href = url
+  const tmp = document.createElement('a');
+  tmp.href = url;
   return browser.cookies.get({
     url: tmp.origin,
     name: 'fstrz',
-  })
+  });
 }
 
 function getFstrzVaryCookie(url) {
   return browser.cookies.get({
     url,
     name: 'fstrz_vary',
-  })
+  });
 }
 
 function getDebugCookie(url) {
   return browser.cookies.get({
     url,
     name: 'frz-debug',
-  })
+  });
 }
 
 function setDebugCookie(url, value) {
-  const tmp = document.createElement('a')
-  tmp.href = url
+  const tmp = document.createElement('a');
+  tmp.href = url;
 
   return browser.cookies.set({
     url: tmp.origin,
     name: 'frz-debug',
     value,
-  })
+  });
 }
 
 function removeFrzVaryCookie(url) {
   return browser.cookies.remove({
     url,
     name: 'fstrz_vary',
-  })
+  });
 }
 
 function reloadPopup(tabID) {
   return browser.tabs
     .reload(tabID, { bypassCache: false })
     .then(() => window.close())
-    .catch(logError)
+    .catch(logError);
 }
 
-;(() => {
+(() => {
   /* global $ */
   // get the current tab's ID and extract request info
   // from the extension object
   const queryInfo = {
     active: true,
     windowId: browser.windows.WINDOW_ID_CURRENT,
-  }
+  };
 
   browser.tabs.query(queryInfo).then(tabs => {
-    const tabID = tabs[0].id
+    const tabID = tabs[0].id;
     // get the extension's window object
-    const extensionWindow = browser.extension.getBackgroundPage()
-    const request = extensionWindow.requests[tabID]
+    const extensionWindow = browser.extension.getBackgroundPage();
+    const request = extensionWindow.requests[tabID];
 
-    $('#smartcache-toggle').hide()
+    $('#smartcache-toggle').hide();
 
     if (request.headers['x-fstrz']) {
-      const explanation = []
+      const explanation = [];
       for (const flag in request.status) {
         explanation.push(
           `<a class="btn btn-default" data-toggle="tooltip" data-placement="top" title="${
             request.status[flag]
           }">${flag}</a>`
-        )
+        );
         if (flag === 'sc') {
-          $('#smartcache-toggle').show()
+          $('#smartcache-toggle').show();
         }
       }
-      $('#x-fstrz-explanation').html(explanation.join(' '))
+      $('#x-fstrz-explanation').html(explanation.join(' '));
 
       getFstrzCookie(request.details.url)
         .then(fstrzCookie => {
-          if (
-            (fstrzCookie && fstrzCookie.value === 'false') ||
-            request.headers['x-fstrz'].indexOf('Z') >= 0
-          ) {
-            $('#fstrz-false').hide()
+          if ((fstrzCookie && fstrzCookie.value === 'false') || request.headers['x-fstrz'].indexOf('Z') >= 0) {
+            $('#fstrz-false').hide();
           } else {
-            $('#fstrz-true').hide()
+            $('#fstrz-true').hide();
           }
 
-          $('#cookie-fstrz').val(fstrzCookie && fstrzCookie.value)
+          $('#cookie-fstrz').val(fstrzCookie && fstrzCookie.value);
         })
-        .catch(logError)
+        .catch(logError);
 
       getFstrzVaryCookie(request.details.url)
         .then(fstrzVaryCookie => {
-          $('#cookie-fstrz-vary').val(fstrzVaryCookie && fstrzVaryCookie.value)
+          $('#cookie-fstrz-vary').val(fstrzVaryCookie && fstrzVaryCookie.value);
         })
-        .catch(logError)
+        .catch(logError);
 
       getDebugCookie(request.details.url)
         .then(debugCookie => {
           if (debugCookie && debugCookie.value === 'true') {
-            $('#enable-trace').hide()
+            $('#enable-trace').hide();
           } else {
-            $('#disable-trace').hide()
+            $('#disable-trace').hide();
           }
         })
-        .catch(logError)
+        .catch(logError);
 
       request
         .getFrzFlags()
         .then(flags => {
           for (flag in flags) {
-            document.getElementById(flag).checked = flags[flag]
+            document.getElementById(flag).checked = flags[flag];
           }
-          $('#testscookies table').show()
+          $('#testflags table').show();
         })
-        .catch(logError)
+        .catch(logError);
     } else {
-      $('#section-middle').hide()
-      $('#section-bottom').hide()
+      $('#section-middle').hide();
+      $('#section-bottom').hide();
     }
 
     if (request.pluggedToCDN()) {
       $('#cdn_status').html(
-        request.servedByCDN()
-          ? '<a class="btn btn-default">HIT</a>'
-          : '<a class="btn btn-default">MISS</a>'
-      )
+        request.servedByCDN() ? '<a class="btn btn-default">HIT</a>' : '<a class="btn btn-default">MISS</a>'
+      );
     } else {
-      $('#cdn_div').hide()
+      $('#cdn_div').hide();
     }
 
-    $('#x-unique-id').val(request.headers['x-unique-id'])
-    $('#cache-control').val(request.headers['cache-control'])
-    $('#statusExplanation').text(request.computeExplanation())
-    $('#statusCode').text(request.headers.status || request.details.statusCode)
-    $('#pop').text(request.findPop())
-    $('#ip').text(request.ip)
+    $('#x-unique-id').val(request.headers['x-unique-id']);
+    $('#cache-control').val(request.headers['cache-control']);
+    $('#statusExplanation').text(request.computeExplanation());
+    $('#statusCode').text(request.headers.status || request.details.statusCode);
+    $('#pop').text(request.findPop());
+    $('#ip').text(request.ip);
 
     $('#fstrz-true').on('click', () => {
       setFstrzCookie(request.details.url, 'true')
         .then(() => {
-          return reloadPopup(tabID)
+          return reloadPopup(tabID);
         })
-        .catch(logError)
-    })
+        .catch(logError);
+    });
 
     $('#fstrz-false').on('click', () => {
       setFstrzCookie(request.details.url, 'false')
         .then(() => {
-          return removeFrzVaryCookie(request.details.url)
+          return removeFrzVaryCookie(request.details.url);
         })
         .then(() => {
-          return reloadPopup(tabID)
+          return reloadPopup(tabID);
         })
-        .catch(logError)
-    })
+        .catch(logError);
+    });
 
     $('#enable-trace').on('click', () => {
       setDebugCookie(request.details.url, 'true').then(() => {
-        return reloadPopup(tabID)
-      })
-    })
+        return reloadPopup(tabID);
+      });
+    });
 
     $('#disable-trace').on('click', () => {
       setDebugCookie(request.details.url, 'false').then(() => {
-        return reloadPopup(tabID)
-      })
-    })
+        return reloadPopup(tabID);
+      });
+    });
 
     $('.copy-button').on('click', function(evt) {
-      const $el = $(this)
-      const copyId = $el.data('copyId')
-      const $copyEl = $(`#${copyId}`)
+      const $el = $(this);
+      const copyId = $el.data('copyId');
+      const $copyEl = $(`#${copyId}`);
 
-      $copyEl.select()
-      document.execCommand('copy')
+      $copyEl.select();
+      document.execCommand('copy');
 
-      evt.preventDefault()
-    })
+      evt.preventDefault();
+    });
 
-    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="tooltip"]').tooltip();
 
     $('#highlightFragments').on('click', () => {
-      request.highlightFragments()
-    })
+      request.highlightFragments();
+    });
 
     $('#showLazyloadedImages').on('click', () => {
-      request.showLazyloadedImages()
-    })
+      request.showLazyloadedImages();
+    });
 
-    document
-      .getElementById('feature-flag-table')
-      .addEventListener('click', function(e) {
-        if (e.target && e.target.nodeName == 'INPUT') {
-          toggleFlag(e.target.id)
-        }
-      })
+    document.getElementById('feature-flag-table').addEventListener('click', function(e) {
+      if (e.target && e.target.nodeName == 'INPUT') {
+        toggleFlag(e.target.id);
+      }
+    });
 
     function toggleFlag(featureFlagName) {
       const toggle = document.getElementById(featureFlagName).checked,
@@ -250,52 +244,47 @@ function reloadPopup(tabID) {
         });
       }
 
-      browser.tabs.update(tabID, { url: newUrl }).catch(logError)
+      browser.tabs.update(tabID, { url: newUrl }).catch(logError);
     }
 
     $('#getFragments').on('click', () => {
       request.getFragments().then(fragments => {
         fragments &&
           fragments.forEach((fragment, index) => {
-            const code = document.createElement('code')
-            code.classList.add('html')
-            const h3 = document.createElement('h3')
-            const pre = document.createElement('pre')
-            code.innerText = fragment
-            pre.appendChild(code)
+            const code = document.createElement('code');
+            code.classList.add('html');
+            const h3 = document.createElement('h3');
+            const pre = document.createElement('pre');
+            code.innerText = fragment;
+            pre.appendChild(code);
 
-            const re = /data-fstrz-fragment-selector="([^"]*)"/
-            h3.innerText = `${index + 1} - ${re.exec(fragment)[1]}`
-            document.getElementById('fragments_div').appendChild(h3)
-            document.getElementById('fragments_div').appendChild(pre)
-          })
-      })
-    })
+            const re = /data-fstrz-fragment-selector="([^"]*)"/;
+            h3.innerText = `${index + 1} - ${re.exec(fragment)[1]}`;
+            document.getElementById('fragments_div').appendChild(h3);
+            document.getElementById('fragments_div').appendChild(pre);
+          });
+      });
+    });
 
     browser.storage.local
       .get('disable-fasterize-cache')
       .then(res => {
-        $('#disable-fasterize-cache').prop(
-          'checked',
-          res && res['disable-fasterize-cache']
-        )
+        $('#disable-fasterize-cache').prop('checked', res && res['disable-fasterize-cache']);
       })
-      .catch(logError)
+      .catch(logError);
 
     $('#disable-fasterize-cache').change(function() {
-      browser.storage.local
-        .set({ 'disable-fasterize-cache': this.checked })
-        .catch(logError)
+      browser.storage.local.set({ 'disable-fasterize-cache': this.checked }).catch(logError);
       browser.runtime
         .sendMessage({
           action: 'update-settings',
           'disable-fasterize-cache': this.checked,
         })
-        .catch(logError)
-    })
+        .catch(logError);
+    });
 
     $('#show-deferjs-debug').on('click', () => {
-      request.getDeferjsDebug()
-    })
-  })
-})()
+      request.getDeferjsDebug();
+    });
+  });
+})();
