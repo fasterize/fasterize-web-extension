a609b80fa7d8b106d67e0844064af9e6665aca65
Julien Gestin <julien.gestin@fasterize.com>
2024-02-12 14:54:41 +0100

    update release

--- README.md
+++ README.md
@@ -65,6 +65,13 @@ Manifest for firefox :
 "frz-request.js",
 "main.js"
 ]
+},
+"browser_specific_settings": {
+"gecko": {
+"id": "{c1687a9a-9054-430e-94cf-2ef9b3caeb7b}",
+"update_url": "https://raw.githubusercontent.com/fasterize/fasterize-web-extension/master/app/update-manifest.json",
+"strict_min_version": "48.0"
+}
 }``
 
 
--- release.js
+++ release.js
@@ -5,21 +5,21 @@ const fsExtra = require('fs-extra');
 const version = process.argv[2];
 
 if (
-  !version ||
-  !process.env['MOZILLA_API_KEY'] ||
-  !process.env['MOZILLA_API_SECRET'] ||
-  !process.env['CHROME_WEBSTORE_ID'] ||
-  !process.env['CHROME_WEBSTORE_SECRET'] ||
-  !process.env['CHROME_WEBSTORE_REFRESH']
+    !version ||
+    !process.env['MOZILLA_API_KEY'] ||
+    !process.env['MOZILLA_API_SECRET'] ||
+    !process.env['CHROME_WEBSTORE_ID'] ||
+    !process.env['CHROME_WEBSTORE_SECRET'] ||
+    !process.env['CHROME_WEBSTORE_REFRESH']
 ) {
-  console.log(`
+    console.log(`
 Usage: MOZILLA_API_KEY=X \
 MOZILLA_API_SECRET=X \
 CHROME_WEBSTORE_ID=X \
 CHROME_WEBSTORE_SECRET=X \
 CHROME_WEBSTORE_REFRESH=X \
 node release.js {version}`);
-  process.exit(1);
+    process.exit(1);
 }
 
 const packageLocation = './package.json';
@@ -47,19 +47,19 @@ console.log('-> update update-manifest.json');
 const appUpdateManifest = require(appUpdateManifestLocation);
 const key = Object.keys(appUpdateManifest.addons)[0];
 appUpdateManifest.addons[key].updates.push({
-  version: version,
-  update_link: `https://github.com/fasterize/fasterize-web-extension/releases/download/${version}/fasterize_status-${version}.xpi`,
+    version: version,
+    update_link: `https://github.com/fasterize/fasterize-web-extension/releases/download/${version}/fasterize_status-${version}.xpi`,
 });
 fs.writeFileSync(appUpdateManifestLocation, JSON.stringify(appUpdateManifest, null, 2));
 
 console.log('-> update README');
 let readme = fs.readFileSync('./README.md');
 readme = readme
-  .toString()
-  .replace(
-    /\[Firefox\]\(.*\)/,
-    `[Firefox](https://github.com/fasterize/fasterize-web-extension/releases/download/${version}/fasterize_status-${version}.xpi)`
-  );
+    .toString()
+    .replace(
+        /\[Firefox\]\(.*\)/,
+        `[Firefox](https://github.com/fasterize/fasterize-web-extension/releases/download/${version}/fasterize_status-${version}.xpi)`
+    );
 fs.writeFileSync(readmeLocation, readme);
 
 console.log(`-> commit changes, tag with the version ${version}`);
@@ -68,24 +68,45 @@ childProcess.execSync(`git tag ${version}`);
 childProcess.execSync(`git push origin --tags && git push`);
 
 console.log('-> publish on Chrome Web Store');
-childProcess.execSync('grunt', { stdio: 'inherit' });
+childProcess.execSync('grunt', {stdio: 'inherit'});
 
 console.log('-> sign addon on Mozilla Addon Store');
 fsExtra.copySync('app', 'tmp');
 const appManifestFirefox = require('./tmp/manifest.json');
 // incognito split is not allowed on firefox.
 delete appManifestFirefox.incognito;
+// Firefox can open popup with openPopup() method. And we need to ask permission to access all urls.
+appManifestFirefox.action = {
+    "default_title": "Fasterize",
+    "default_icon": "icons/store/icon.png"
+};
+// Firefox not support yet the service worker
+appManifestFirefox.background = {
+    "scripts": [
+        "mapping.js",
+        "frz-request.js",
+        "main.js"
+    ]
+};
+// Chrome not support browser_specific_settings
+appManifestFirefox.browser_specific_settings = {
+    "gecko": {
+        "id": "{c1687a9a-9054-430e-94cf-2ef9b3caeb7b}",
+        "update_url": "https://raw.githubusercontent.com/fasterize/fasterize-web-extension/master/app/update-manifest.json",
+        "strict_min_version": "48.0"
+    }
+}
 fs.writeFileSync('./tmp/manifest.json', JSON.stringify(appManifestFirefox, null, 2));
 
-childProcess.execSync('npx web-ext -a dist/firefox -s tmp build --overwrite-dest', { stdio: 'inherit' });
+childProcess.execSync('npx web-ext -a dist/firefox -s tmp build --overwrite-dest', {stdio: 'inherit'});
 childProcess.execSync(
-  `npx web-ext -a dist/firefox -s tmp sign --api-key=${process.env['MOZILLA_API_KEY']} \
+    `npx web-ext -a dist/firefox -s tmp sign --api-key=${process.env['MOZILLA_API_KEY']} \
   --api-secret=${process.env['MOZILLA_API_SECRET']} --timeout 600000`,
-  { stdio: 'inherit' }
+    {stdio: 'inherit'}
 );
 
 console.log('-> publish mozilla addon on Github releases');
 childProcess.execSync(
-  `hub release create -a dist/firefox/fasterize_status-${version}.xpi -m "Release ${version}" ${version}`,
-  { stdio: 'inherit' }
+    `hub release create -a dist/firefox/fasterize_status-${version}.xpi -m "Release ${version}" ${version}`,
+    {stdio: 'inherit'}
 );
